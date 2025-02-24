18c6b1f8d6c74b9e1b796927213324cc161c064d
Shruthi L <Shruthi@agpl.net>
2025-02-24 17:37:34 +0530

    worked on basics of php

---
+++ 2.data_types.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+
+    //scalar types
+    $string = "shruti";
+    $int = 12345;
+    $float = 3.47;
+    $bool = true;
+
+    //Array type
+    $names = ["Shruti", "Kavya", "Netu", "Deepa"];
+
+    echo "$string";
+    echo "<br>";
+    echo "$int";
+    echo "<br>";
+    echo "$float";
+    echo "<br>";
+    echo "$bool";
+
+    ?>
+</body>
+
+</html>
---
+++ 3.built-in-variable.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    // echo $_SERVER["DOCUMENT_ROOT"];
+    // echo "<br>";
+    // echo $_SERVER["PHP_SELF"];
+    // echo "<br>";
+    // echo $_SERVER["SERVER_NAME"];
+    // echo "<br>";
+    // echo $_SERVER["REQUEST_METHOD"];
+
+    //get method
+    // echo $_GET["name"];
+    // echo $_GET["age"];
+
+    //request method
+    // echo $_REQUEST[""];
+
+    //files 
+    // echo $_FILES[""];
+
+    //cookie
+    // echo $_COOKIE[""];
+
+    //session
+    // $_SESSION["username"] = "Shruti";
+    // echo $_SESSION["username"];
+
+    //envirinment
+    $_ENV[""];
+
+    ?>
+
+</body>
+
+</html>
