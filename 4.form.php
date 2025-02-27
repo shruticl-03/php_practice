a06bffd16ab7d9b391fd6de8c6e4b620b259364f
Shruthi L <Shruthi@agpl.net>
2025-02-27 17:03:57 +0530

    added form file

---
+++ 4.form.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP</title>
+</head>
+<body>
+    <main>
+        <form action="includes/formhandler.php" method="post"> 
+            <label for="firstname">First Name</label>
+            <input type="text" name="firstname" id="firstname" placeholder="Firstname...">
+
+            <label for="lastname">Last Name</label>
+            <input type="text" name="lastname" id="lastname" placeholder="Lastname...">
+
+            <label for="favouritefruit">Favourite Fruit?</label>
+            <select name="favouritefruit" id="favouritefruit">
+                <option value="none">None</option>
+                <option value="apple">Apple</option>
+                <option value="grapes">Grapes</option>
+                <option value="mango">Mango</option>
+                <option value="orange">Orange</option>
+                <option value="pineapple">Pineapple</option>
+            </select>
+            <button type="submit">Submit</button>
+
+
+        </form>
+    </main>
+    
+</body>
+</html>
